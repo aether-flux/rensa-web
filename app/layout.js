c65ac0340787db070ebcedffc0541d6ecfc40c00
xerctia <work.amar05@yahoo.com>
2025-03-31 00:24:29 +0530

    add google analytics

--- app/layout.js
+++ app/layout.js
@@ -88,6 +88,23 @@ export default function RootLayout({ children }) {
       <body
         className={`${clashDisplay.variable} ${clashGrotesk.variable} ${jetbrainsMono.variable} antialiased`}
       >
+
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-87K6WYLCJY`}
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-87K6WYLCJY', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+
+
         {children}
       </body>
     </html>
